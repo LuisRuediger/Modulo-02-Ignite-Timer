59ea89ba84064afa857e37b2036392e424c61891
Luis Ruediger <luisruediger27@gmail.com>
2023-03-06 14:16:13 -0300

    remove html font-size for rem

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,10 +7,6 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  html {
-    font-size: 62.5%;
-  }
-
   :focus {
     outline: 0;
     box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']}
@@ -24,6 +20,6 @@ export const GlobalStyle = createGlobalStyle`
   body, input, textarea, button {
     font-family: 'Roboto', sans-serif;
     font-weight: 400;
-    font-size: 1.6rem;
+    font-size: 1rem;
   }
 `
